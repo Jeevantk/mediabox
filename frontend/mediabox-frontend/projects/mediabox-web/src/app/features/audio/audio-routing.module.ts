04fa5543af080b6caae1cd13d977e6b779ce9da7
jeevantk <jeevan.thomaskoshy@gmail.com>
2022-12-11 20:15:35 +0530

    search feature added

--- frontend/mediabox-frontend/projects/mediabox-web/src/app/features/audio/audio-routing.module.ts
+++ frontend/mediabox-frontend/projects/mediabox-web/src/app/features/audio/audio-routing.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AudioListComponent } from './audio-list/audio-list.component';
-import { AudioUploadComponent } from './audio-upload/audio-upload.component';
 
 const routes: Routes = [
-    { 
-        path: 'upload', 
-        component: AudioUploadComponent,
-        pathMatch: 'full'
-    },
     { 
         path: '', 
         component: AudioListComponent
